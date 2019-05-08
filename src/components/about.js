aaeccf4b917dfb1e99a8e7de63c369e8adde95f5
allyjfuller <allyjfuller@gmail.com>
2019-05-07 23:21:23 -0400

    final css commit

--- src/components/about.js
+++ src/components/about.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import './about.css'
 
 export function About() {
 	return(
 
-		<div>
-			<h2>About Us</h2>
+		<div className="about-menew">
+			<h2 className="about">About Menew</h2>
+			<p className="about-content">
+			Menew is an app that keeps you up to date on menu changes from your favorite restaurants.
+			<br />
+			Restaurants and bars can create an account to post their new items for patrons to see on the front page.
+			<br />
+			When is the spring cocktail menu launching? Follow on Menew. What are the Thanksgiving specials? Follow on Menew.
+			Keep your loyal customers in the first-to-know!
+			</p>
 		</div>
 
 	);
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import './contact.css'
 
 export function Contact() {
 	return(
 
-		<div>
+		<div className="contact">
 			<h2>Contact</h2>
+			<p className="contact-content">For questions and inquiries please contact
+			<br />
+			Email: allyjfuller@gmail.com
+			<br />
+			Phone: (917) 968-5487
+			</p>
 		</div>
 
 	);
